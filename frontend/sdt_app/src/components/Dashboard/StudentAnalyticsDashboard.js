d6958b8b4830e5734c962653ae1c079ac6d65374
Maderi <maderidemeyer@gmail.com>
2025-08-20 13:51:22 +0200

    Revert "Implemented login page"
    This reverts commit 54ee9af1f0c8eb7a6079fd18a901f9c2073dbffa.

--- frontend/sdt_app/src/components/Dashboard/StudentAnalyticsDashboard.js
+++ frontend/sdt_app/src/components/Dashboard/StudentAnalyticsDashboard.js
@@ -10,7 +10,7 @@ import ErrorMessage from '../UI/ErrorMessage';
 // Chart components
 import MetricsGrid from './MetricsGrid';
 import ActivityChart from './ActivityChart';
-import ActivityBreakDown from './ActivityBreakDown';
+import ActivityBreakDown from './ActivityBreakdown';
 import ActivityBarChart from './ActivityBarChart';
 import StudentSelector from './StudentSelector';
 import ApiConnectionGuide from './ApiConnectionGuide';
